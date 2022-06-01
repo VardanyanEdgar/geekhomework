5a861a7b6d22f80aa914c9cf8ea9fbe0c30e4be8
VardanyanEdgar <100074520+VardanyanEdgar@users.noreply.github.com>
2022-06-02 01:39:05 +0300

    Add files via upload

---
+++ oop 1.2.cpp
@@ -0,0 +1,52 @@
+﻿/*
+* 2. Написать класс с именем RGBA, который содержит 4 переменные-члена типа
+std::uint8_t: m_red, m_green, m_blue и m_alpha (#include cstdint для доступа к этому типу).
+Задать 0 в качестве значения по умолчанию для m_red, m_green, m_blue и 255 для m_alpha.
+Создать конструктор со списком инициализации членов, который позволит пользователю
+передавать значения для m_red, m_blue, m_green и m_alpha. Написать функцию print(),
+которая будет выводить значения переменных-членов.
+*/
+
+#include <iostream>
+#include <cstdint>
+
+class RGBA {
+private:
+    std::uint8_t m_red = 0;
+    std::uint8_t m_green = 0;
+    std::uint8_t m_blue = 0;
+    std::uint8_t m_alpha = 256;
+
+public:
+    RGBA(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha)
+        : m_red(red), m_green(green), m_blue(blue), m_alpha(alpha)
+    {
+
+    }
+
+    void print()
+    {
+        std::cout << "m_red: " << m_red << "m_green: " << m_green << "m_blue: " << m_blue << "m_alpha: " << m_alpha;
+    }
+
+};
+
+/*Date(int day, int month, char* dayOfWeek = "mon")
+    : m_day(day), m_month(month), m_dayOfWeek(dayOfWeek) // напрямую
+    инициализируем переменные - члены класса*/
+
+int main()
+{
+    int red, green, blue, alpha;
+    std::cout << "please insert number for red" << std::endl;
+    std::cin >> red;
+    std::cout << "please insert number for green" << std::endl;
+    std::cin >> green;
+    std::cout << "please insert number for blue" << std::endl;
+    std::cin >> blue;
+    std::cout << "please insert number for alpha" << std::endl;
+    std::cin >> alpha;
+    RGBA First(red, green, blue, alpha);
+    First.print();
+    return 0;
+}
---
+++ oop 1.3.cpp
@@ -0,0 +1,63 @@
+﻿/* private-массив целых чисел длиной 10;
+
+• private целочисленное значение для отслеживания длины стека;
+• public-метод с именем reset(), который будет сбрасывать длину и все значения элементов на 0;
+• public-метод с именем push(), который будет добавлять значение в стек.
+push() должен возвращать значение false, если массив уже заполнен, и true в противном случае;
+• public-метод с именем pop() для вытягивания и возврата значения из стека.
+Если в стеке нет значений, то должно выводиться предупреждение;
+• public-метод с именем print(), который будет выводить все значения стека.*/
+
+
+#include <iostream>
+#include <cstdint>
+
+class Stack
+{
+private:
+    static const int Size = 10;
+    int arr[Size] = { 1, 5, 3, 6, 7, 8,  9, 12, 16, 45 };
+
+
+public:
+
+    void reset()
+    {
+        for (int i = 0; i < Size-1; i++)
+        {
+            arr[i] = 0;
+            std::cout << "arr I first is" << arr[i] << std::endl;
+            arr[i] = arr[i + 1];
+            std::cout << "arr I+1 now " << arr[i+1] << std::endl;
+        };
+    };
+
+    void print() {
+        for (int i = 0; i < Size; i++)
+        {
+            std::cout << "arr new now is " << arr[i] << std::endl;
+            std::cout << "sise " << Size << std::endl;
+        }
+    };
+};
+
+        int main()
+        {
+            Stack stack;
+            stack.reset();
+            stack.print();
+
+            //stack.push(3);
+           // stack.push(7);
+           // stack.push(5);
+           // stack.print();
+
+           // stack.pop();
+           // stack.print();
+
+            //stack.pop();
+           // stack.pop();
+            //stack.print();
+
+            return 0;
+        }
